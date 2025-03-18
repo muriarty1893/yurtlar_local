147cdf422c1797830058210e02a424539a91c227
Murat Eker <murateker9166@gmail.com>
2025-03-18 10:44:56 +0300

    initial commit

---
+++ Yurtlar/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Yurtlar.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult YönetimSayfa()
+        {
+            return View(); 
+        }
+
+        public ActionResult OnaySayfam()
+        {
+            return View();
+        }
+
+        public ActionResult UrunleriListele() {
+
+            return View();
+        }
+
+        public ActionResult SatisEkle()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
